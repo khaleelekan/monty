fcde7ef5bbeb59bccb13fc503e4142ba44705816
khaleelekan <kalilulekan@gmail.com>
2023-12-14 07:10:43 -0800

    new

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * f_pall - Prints the stack.
+ * @head: Stack head
+ * @counter: Unused parameter
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+    stack_t *current;
+    (void)counter;
+
+    current = *head;
+    if (current == NULL)
+        return;
+
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
